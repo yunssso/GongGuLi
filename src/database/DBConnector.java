95c37c6a3f9c028ba89a76dcf6568fd8791ff9d5
yunnb <chldbsrud010@naver.com>
2023-10-04 22:41:49 +0900

    DB 통일

--- src/database/DBConnector.java
+++ src/database/DBConnector.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 // DB에 접속하는 코드
 public class DBConnector {
     private static String driver = "org.mariadb.jdbc.Driver";  // jdbc 드라이버.
-    private static String url = "jdbc:mariadb://localhost:3306/test";      // DB 서버 주소 ( 이 주소는 로컬 주소 ) ( 나중에 바꿔야됨 )
+    private static String url = "jdbc:mariadb://localhost:3306/GongGuLi";      // DB 서버 주소 ( 이 주소는 로컬 주소 ) ( 나중에 바꿔야됨 )
     private static String user = "root";       // 서버에 접속하는 유저 이름 ( 서버에서 권한 주는 식인듯? )
     private static String password = "123456";     // 서버 비밀번호
 
