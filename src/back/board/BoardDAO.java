a9b1af7fdec7a19228cb6d43a8741bd100564208
YoooooonB <zxc291142044@gmail.com>
2023-11-09 15:35:34 +0900

    e

--- src/back/board/BoardDAO.java
+++ src/back/board/BoardDAO.java
@@ -1,5 +1,6 @@
 package back.board;
 
+import back.user.UserDTO;
 import database.DBConnector;
 
 import java.sql.Connection;
@@ -13,11 +14,28 @@ public class BoardDAO {
     Connection conn = null;
     PreparedStatement pt = null;
     ResultSet rs = null;
-    public String[][] selectAll() {
+
+    // arraylist에 역순으로 담은 뒤, 2차원 배열로 저장 및 전달
+    // 전체 개시글
+    public String[][] printBoard(String region, String category) {
+        // 역순으로 리스트에 담기
         List<BoardDTO> list = new ArrayList<>();
         conn = DBConnector.getConnection();
-        String selectSQL = "SELECT * FROM board ORDER BY postingTime DESC"; // 역순으로 리스트에 담기
         try {
+            String selectSQL = "";
+            if (region.equals(" --") && !category.equals(" --")) {
+                selectSQL = "SELECT * FROM board WHERE category = ? ORDER BY postingTime DESC;";
+                pt.setString(1, category);
+            } else if (!region.equals(" --") && category.equals(" --")) {
+                selectSQL = "SELECT * FROM board WHERE region = ? ORDER BY postingTime DESC;";
+                pt.setString(1, region);
+            } else if (!region.equals(" --") && !category.equals(" --")) {
+                selectSQL = "SELECT * FROM board WHERE region = ? WHERE category = ? ORDER BY postingTime DESC;";
+                pt.setString(1, region);
+                pt.setString(2, category);
+            } else {
+                selectSQL = "SELECT * FROM board ORDER BY postingTime DESC";
+            }
             pt = conn.prepareStatement(selectSQL);
             rs = pt.executeQuery();
             while (rs.next()) {
@@ -43,10 +61,52 @@ public class BoardDAO {
         String[][] data = new String[list.size()][];    // ArrayList에 저장한 데이터들 2차원 배열로 변환해주기.
 
         for (int i = 0; i < list.size(); i++) {
-            BoardDTO dto = list.get(i);
-            data[i] = new String[]{dto.getRegion(), dto.getCategory(), dto.getTitle(), dto.getNickName(), dto.getPeopleNum()};
+            BoardDTO boardDTO = list.get(i);
+            data[i] = new String[]{boardDTO.getRegion(), boardDTO.getCategory(), boardDTO.getTitle(), boardDTO.getNickName(), boardDTO.getPeopleNum()};
         }
         System.out.println("2차원 배열로 변환 완료.");
+
+        return data;
+    }
+
+    // 내가 쓴 게시글
+    public String[][] printMyBoard(UserDTO userDTO) {
+        // 역순으로 리스트에 담기
+        List<BoardDTO> list = new ArrayList<>();
+        conn = DBConnector.getConnection();
+        String selectSQL = "SELECT * FROM board WHERE nickName = ? ORDER BY postingTime DESC;";
+        try {
+            pt = conn.prepareStatement(selectSQL);
+            pt.setString(1, userDTO.getNickName());
+            rs = pt.executeQuery();
+            while (rs.next()) {
+                BoardDTO boardDTO = new BoardDTO();
+                boardDTO.setTitle(rs.getString("title"));
+                boardDTO.setRegion(rs.getString("region"));
+                boardDTO.setCategory(rs.getString("category"));
+                boardDTO.setNickName(rs.getString("nickName"));
+                boardDTO.setPeopleNum(rs.getString("peopleNum"));
+                boardDTO.setContent(rs.getString("content"));
+
+                list.add(boardDTO);
+            }
+            System.out.println("내 글 데이터 ArrayList에 저장 완료.");
+
+            rs.close();
+            pt.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("내 글 ArrayList 저장 중 오류 발생.");
+        }
+
+        String[][] data = new String[list.size()][];    // ArrayList에 저장한 데이터들 2차원 배열로 변환해주기.
+
+        for (int i = 0; i < list.size(); i++) {
+            BoardDTO boardDTO = list.get(i);
+            data[i] = new String[]{boardDTO.getRegion(), boardDTO.getCategory(), boardDTO.getTitle(), boardDTO.getPeopleNum()};
+        }
+        // 지역 카테고리 제목 현황
+        System.out.println("내 글 2차원 배열로 변환 완료.");
 
         return data;
     }
@@ -55,13 +115,14 @@ public class BoardDAO {
         selectRow++;
         BoardDTO boardDTO = new BoardDTO();
         String selectSQL = "SELECT * FROM boardView WHERE num = ?";
-        String updateSQL = "UPDATE board SET view = view + 1 WHERE title = ? AND region = ? AND category = ? AND nickName = ? AND peopleNum = ? AND content = ?";
+        String updateSQL = "UPDATE board SET view = view + 1 WHERE boardID = ?";
         try {
             conn = DBConnector.getConnection();
             pt = conn.prepareStatement(selectSQL);
             pt.setInt(1, selectRow);
             rs = pt.executeQuery();
             if (rs.next()) {
+                boardDTO.setBoardId(rs.getInt("boardID"));
                 boardDTO.setTitle(rs.getString("title"));
                 boardDTO.setRegion(rs.getString("region"));
                 boardDTO.setCategory(rs.getString("category"));
@@ -73,12 +134,7 @@ public class BoardDAO {
             System.out.println("자세히 보기 성공.");
 
             pt = conn.prepareStatement(updateSQL);
-            pt.setString(1, boardDTO.getTitle());
-            pt.setString(2, boardDTO.getRegion());
-            pt.setString(3, boardDTO.getCategory());
-            pt.setString(4, boardDTO.getNickName());
-            pt.setString(5, boardDTO.getPeopleNum());
-            pt.setString(6, boardDTO.getContent());
+            pt.setInt(1, boardDTO.getBoardId());
             pt.execute();
 
             rs.close();
@@ -90,10 +146,36 @@ public class BoardDAO {
 
         try {
             conn = DBConnector.getConnection();
-
-
             System.out.println("조회수 1 증가");
+            pt.close();
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return boardDTO;
+    }
+    public BoardDTO readMoreMyPost(int selectRow) {   // 게시글 자세히 보기
+        selectRow++;
+        BoardDTO boardDTO = new BoardDTO();
+        String selectSQL = "SELECT * FROM boardView WHERE num = ?";
+        try {
+            conn = DBConnector.getConnection();
+            pt = conn.prepareStatement(selectSQL);
+            pt.setInt(1, selectRow);
+            rs = pt.executeQuery();
+            if (rs.next()) {
+                boardDTO.setBoardId(rs.getInt("boardID"));
+                boardDTO.setTitle(rs.getString("title"));
+                boardDTO.setRegion(rs.getString("region"));
+                boardDTO.setCategory(rs.getString("category"));
+                boardDTO.setNickName(rs.getString("nickName"));
+                boardDTO.setPeopleNum(rs.getString("peopleNum"));
+                boardDTO.setContent(rs.getString("content"));
+                boardDTO.setView(rs.getInt("view") + 1);
+            }
+            System.out.println("자세히 보기 성공.");
 
+            rs.close();
             pt.close();
             conn.close();
         } catch (Exception e) {
