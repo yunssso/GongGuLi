52ff10a6d6551fa80b9e71a10625fc76e2295bdc
YoooooonB <zxc291142044@gmail.com>
2023-12-03 04:29:33 +0900

    채팅 참여 시 chattingMember 테이블에 값 저장

--- src/back/dao/chatting/JoinChattingRoomDAO.java
+++ src/back/dao/chatting/JoinChattingRoomDAO.java
@@ -27,24 +27,49 @@ public class JoinChattingRoomDAO {
             System.out.println("채팅방이 가득 참");
             return 0;   //  채팅방 인원이 가득 참
         } else {
-            try {
-                conn = DBConnector.getConnection();
-                String insertSQL = "INSERT INTO chattingMember (port, memberUuid) VALUES (?, ?);";
-                pt = conn.prepareStatement(insertSQL);
-                pt.setInt(1, port);
-                pt.setString(2, uuid);
+            if (isJoinedChat(port, uuid)) {
+                try {
+                    conn = DBConnector.getConnection();
+                    String insertSQL = "INSERT INTO chattingMember (port, memberUuid) VALUES (?, ?);";
+                    pt = conn.prepareStatement(insertSQL);
+                    pt.setInt(1, port);
+                    pt.setString(2, uuid);
 
-                if (!pt.execute()) {
-                    pt.close();
-                    conn.close();
-                    System.out.println("입장 성공");
-                    return 1;   //  채팅방 입장 성공
+                    if (!pt.execute()) {
+                        pt.close();
+                        conn.close();
+                        System.out.println("입장 성공");
+                        return 1;   //  채팅방 입장 성공
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
+            } else {
+                System.out.println("채팅방에 이미 존재2");
+                return 1;
             }
         }
         System.out.println("에러");
         return -1;  //  에러
+    }
+
+//    채팅방에 이미 존재 하는지 여부 확인
+    public boolean isJoinedChat(int port, String uuid) {
+        try {
+            conn = DBConnector.getConnection();
+            String selectSQL = "SELECT EXISTS(select * from chattingMember where port = ? and memberUuid = ?) as cnt;";
+            pt = conn.prepareStatement(selectSQL);
+            pt.setInt(1, port);
+            pt.setString(2, uuid);
+            rs = pt.executeQuery();
+            rs.next();
+            if (rs.getInt(1) == 1) {
+                System.out.println("채팅방에 이미 존재합니다.");
+                return false;   //  채팅방에 이미 존재 (디비에 중복으로 저장돼서 구분해야함.)
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;    //  채팅방에 존재하지 않음 (신규 입장 유저)
     }
 }
