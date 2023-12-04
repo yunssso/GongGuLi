fe7ac86bf087fed6af112cefa0929e646f11ef30
yoooooonB <zxc291142044@gmail.com>
2023-12-04 16:02:50 +0900

    AWS RDS

--- src/database/DBConnector.java
+++ src/database/DBConnector.java
@@ -6,11 +6,11 @@ import java.sql.DriverManager;
 // DB에 접속하는 코드
 public class DBConnector {
     private static String driver = "org.mariadb.jdbc.Driver";  // jdbc 드라이버.
-    private static String url = "jdbc:mariadb://localhost:3306/GongGuLi";      // DB 서버 주소 ( 이 주소는 로컬 주소 )
-//    private static String url = "jdbc:mariadb://gongguli.c5bclsnfmtbe.ap-northeast-2.rds.amazonaws.com:3306/GongGuLi";      // RDS DB 서버 주소
     private static String user = "root";       // 서버에 접속하는 유저 이름 ( 서버에서 권한 주는 식인듯? )
-    private static String password = "123456";     // 서버 비밀번호
-//    private static String password = "root1234";     // RDS 서버 비밀번호
+//    private static String url = "jdbc:mariadb://localhost:3306/GongGuLi";      // DB 서버 주소 ( 이 주소는 로컬 주소 )
+        private static String url = "jdbc:mariadb://gongguli.c5bclsnfmtbe.ap-northeast-2.rds.amazonaws.com:3306/GongGuLi";      // RDS DB 서버 주소
+//    private static String password = "123456";     // 서버 비밀번호
+    private static String password = "root1234";     // RDS 서버 비밀번호
 
     public static Connection getConnection() {
         Connection con = null;
